96d4369ffeca20e19d162cd15aa683e263d16448
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2026-06-01 17:55:52 +0200

    finished

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -7,6 +7,7 @@ import mate.academy.service.impl.FileReaderServiceImpl;
 import mate.academy.service.impl.ProductParserImpl;
 import mate.academy.service.impl.ProductServiceImpl;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +17,7 @@ public class Injector {
                     ProductParser.class, ProductParserImpl.class,
                     FileReaderService.class, FileReaderServiceImpl.class);
 
-    private final Map<Class<?>,Object> instances = new HashMap<>();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
 
     private static final Injector injector = new Injector();
 
@@ -42,5 +43,27 @@ public class Injector {
                     "@Component annotation on the class "
                     + clazz.getName());
         }
+
+        Object instance;
+
+        try {
+            instance = clazz.getDeclaredConstructor().newInstance();
+
+            for (Field field : clazz.getDeclaredFields()) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    Object fieldInstance = getInstance(field.getType());
+
+                    field.setAccessible(true);
+
+                    field.set(instance, fieldInstance);
+                }
+            }
+        } catch (ReflectiveOperationException exception) {
+            throw new RuntimeException("Couldn't create class example "
+            + clazz.getName(), exception);
+        }
+
+        instances.put(clazz, instance);
+        return instance;
     }
 }
